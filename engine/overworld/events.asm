1078a9ff3425bea59af17ce484d559b84e7dd5f2
AngelofSilence1213 <182548478+AngelofSilence1213@users.noreply.github.com>
2024-12-13 23:47:58 +0000

    Pokemon no longer lose HP from poisoning in the overworld

--- engine/overworld/events.asm
+++ engine/overworld/events.asm
@@ -893,10 +893,6 @@ CountStep:
 	call DoRepelStep
 	jr c, .doscript
 
-	; Count the step for poison and total steps
-	ld hl, wPoisonStepCount
-	inc [hl]
-	ld hl, wStepCount
 	inc [hl]
 	; Every 256 steps, increase the happiness of all your Pokemon.
 	jr nz, .skip_happiness
@@ -918,17 +914,6 @@ CountStep:
 	; Increase the EXP of (both) DayCare Pokemon by 1.
 	farcall DayCareStep
 
-	; Every 4 steps, deal damage to all poisoned Pokemon.
-	ld hl, wPoisonStepCount
-	ld a, [hl]
-	cp 4
-	jr c, .skip_poison
-	ld [hl], 0
-
-	farcall DoPoisonStep
-	jr c, .doscript
-
-.skip_poison
 	call DoBikeStep
 
 .done
